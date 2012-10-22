87a45ec9f4a07abf65fb59e409507fbb932040bf
dreadwail <dreadwail42@gmail.com>
2012-10-21 22:03:33 -0700

    Centering of menu text. Does not work as intended in windowed mode, but does in fullscreen. Added a TODO to get it correct in windowed mode.

--- src/org/seattlegamer/spacegame/RenderableText.java
+++ src/org/seattlegamer/spacegame/RenderableText.java
@@ -5,12 +5,15 @@ import java.awt.Font;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
 
+import org.seattlegamer.spacegame.utils.GraphicsUtils;
+
 public class RenderableText implements Renderable {
 
 	private final String text;
 	private final Font font;
 	private int positionX;
 	private int positionY;
+	private boolean centered;
 
 	public RenderableText(String text, Font font) {
 		this.text = text;
@@ -21,6 +24,7 @@ public class RenderableText implements Renderable {
 	
 	public void setPositionX(int x) {
 		this.positionX = x;
+		this.centered = false;
 	}
 	
 	public void setPositionY(int y) {
@@ -28,22 +32,32 @@ public class RenderableText implements Renderable {
 	}
 	
 	public void center() {
-		//TODO:
-//		Dimension currentScreenDimension = GraphicsUtils.getCurrentScreenDimension();
-//		
-//		FontRenderContext context = gfx2d.getFontRenderContext();
-//        Rectangle2D bounds2D = _font.getStringBounds(_widestString, context);
-//        Rectangle bounds = bounds2D.getBounds();
+		this.positionX = 0;
+		this.centered = true;
 	}
 	
 	@Override
 	public void render(Graphics2D graphics) {
+		
+		int posX = this.positionX;
+
+		if(this.centered) {
+			posX = this.determineCenteredX(graphics);
+		}
 
 		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 		graphics.setFont(this.font);
 		graphics.setColor(Color.WHITE);
-		graphics.drawString(this.text, this.positionX, this.positionY);
+		graphics.drawString(this.text, posX, this.positionY);
 
 	}
+	
+	private int determineCenteredX(Graphics2D graphics) {
+		int centerScreenX = GraphicsUtils.getCenterScreenX();
+		int textPixelWidth = GraphicsUtils.getTextPixelWidth(graphics, this.font, this.text);
+		double offset = textPixelWidth / 2;
+		return (int) (centerScreenX - offset);
+	}
+
 
 }
--- src/org/seattlegamer/spacegame/utils/GraphicsUtils.java
+++ src/org/seattlegamer/spacegame/utils/GraphicsUtils.java
@@ -1,7 +1,11 @@
 package org.seattlegamer.spacegame.utils;
 
 import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.Toolkit;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
 
 public class GraphicsUtils {
 
@@ -10,5 +14,18 @@ public class GraphicsUtils {
 		return toolkit.getScreenSize();
 	}
 	
+	//TODO: works if fullscreen, does not work as intended if windowed.
+	public static int getCenterScreenX() {
+		Dimension currentScreenDimension = getCurrentScreenDimension();
+		double screenWidth = currentScreenDimension.getWidth();
+		double centerX = screenWidth / 2;
+		return (int) centerX;
+	}
+	
+	public static int getTextPixelWidth(Graphics2D graphics, Font font, String text) {
+		FontRenderContext context = graphics.getFontRenderContext();
+		Rectangle2D bounds = font.getStringBounds(text, context);
+		return (int) bounds.getWidth();
+	}
 	
 }
