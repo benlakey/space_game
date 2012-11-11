070baca1cfff109b0e3f0d5cecd6352e1c023150
dreadwail <dreadwail42@gmail.com>
2012-11-11 13:07:33 -0800

    Simplifying the renderers. There is no need to draw an iterable collection of renderables, only a single.

--- src/org/seattlegamer/spacegame/CanvasRenderer.java
+++ src/org/seattlegamer/spacegame/CanvasRenderer.java
@@ -2,8 +2,6 @@ package org.seattlegamer.spacegame;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.util.Collection;
-import java.util.LinkedList;
 
 public class CanvasRenderer implements Renderer {
 	
@@ -12,29 +10,15 @@ public class CanvasRenderer implements Renderer {
 	public CanvasRenderer(GameCanvas canvas) {
 		this.canvas = canvas;
 	}
-	
+
 	@Override
 	public void draw(Renderable renderable) {
-		Collection<Renderable> renderables = new LinkedList<Renderable>();
-		renderables.add(renderable);
-		this.draw(renderables);
-	}
-	
-	@Override
-	public void draw(Iterable<? extends Renderable> itemsToRender) {
 		
 		Graphics2D graphics = this.canvas.getGraphics();
 
 		try {
-
 			this.clearScreen(graphics);
-	
-			if(itemsToRender != null) {
-				for(Renderable renderable : itemsToRender) {
-					renderable.render(graphics);
-				}
-			}
-
+			renderable.render(graphics);
 		} finally {
 			graphics.dispose();
 		}
--- src/org/seattlegamer/spacegame/Renderer.java
+++ src/org/seattlegamer/spacegame/Renderer.java
@@ -2,5 +2,4 @@ package org.seattlegamer.spacegame;
 
 public interface Renderer {
 	void draw(Renderable renderable);
-	void draw(Iterable<? extends Renderable> itemsToRender);
 }
