07a81e5c5e6e6fac28aabec069ff87a02ef14056
dreadwail <dreadwail42@gmail.com>
2012-11-20 21:19:45 -0800

    Switching to using a bitmask for the component groups.

--- src/org/seattlegamer/spacegame/Component.java
+++ src/org/seattlegamer/spacegame/Component.java
@@ -1,26 +1,29 @@
 package org.seattlegamer.spacegame;
 
-import java.util.HashSet;
-import java.util.Set;
+import java.util.EnumSet;
 
 public abstract class Component implements Handler {
 
 	protected final Handler owner;
 	private boolean enabled;
-	private Set<ComponentGroup> groups;
+	private EnumSet<ComponentGroup> groupBitMask;
 	
 	public Component(Handler owner) {
 		this.owner = owner;
 		this.enabled = true;
-		this.groups = new HashSet<ComponentGroup>();
-	}
-
-	public void applyGroup(ComponentGroup group) {
-		this.groups.add(group);
+		this.groupBitMask = EnumSet.noneOf(ComponentGroup.class);
 	}
 	
+	public void applyGroup(ComponentGroup group) {
+		this.groupBitMask.add(group);
+    }
+
+    public void removeGroup(ComponentGroup group) {
+    	this.groupBitMask.remove(group);
+    }
+
 	public boolean isMember(ComponentGroup group) {
-		return this.groups.contains(group);
+		return this.groupBitMask.contains(group);
 	}
 
 	public boolean isEnabled() {
